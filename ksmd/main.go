39f39400c7dba534f3153929648132af1042ff92
Damian Gryski <damian@gryski.com>
2013-10-15 20:21:55 +0200

    migrate ksmd to glog and logging levels

--- ksmd/main.go
+++ ksmd/main.go
@@ -9,8 +9,8 @@ import (
 	"flag"
 	"fmt"
 	"github.com/conformal/yubikey"
+	"github.com/golang/glog"
 	_ "github.com/mattn/go-sqlite3"
-	"log"
 	"net/http"
 	"os"
 )
@@ -23,13 +23,13 @@ func decryptHandler(w http.ResponseWriter, r *http.Request) {
 	otp := r.FormValue("otp")
 
 	if otp == "" {
-		log.Println("ERR No OTP provided")
+		glog.Info("ERR No OTP provided")
 		http.Error(w, "ERR No OTP provided", http.StatusOK)
 		return
 	}
 
 	if len(otp) < 32 || len(otp) > 48 || !yubikey.ModHexP([]byte(otp)) {
-		log.Println("ERR Invalid OTP format:", otp)
+		glog.Info("ERR Invalid OTP format: ", otp)
 		http.Error(w, "ERR Invalid OTP format", http.StatusOK)
 		return
 	}
@@ -42,7 +42,7 @@ func decryptHandler(w http.ResponseWriter, r *http.Request) {
 
 	stmt, err := KeysDB.Prepare("SELECT aeskey, internalname FROM yubikeys WHERE publicname = ? AND active = 1")
 	if err != nil {
-		log.Println("ERR Database error:", err)
+		glog.Error("ERR DB error during Prepare(): ", err)
 		http.Error(w, "ERR Database error", http.StatusOK)
 		return
 	}
@@ -52,10 +52,10 @@ func decryptHandler(w http.ResponseWriter, r *http.Request) {
 	err = stmt.QueryRow(yid).Scan(&aeskeyHex, &name)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			log.Println("ERR Unknown yubikey:", yid)
+			glog.Info("ERR Unknown yubikey: ", yid)
 			http.Error(w, "ERR Unknown yubikey", http.StatusOK)
 		} else {
-			log.Println("ERR Database error:", err)
+			glog.Error("ERR DB error during SELECT: ", err)
 			http.Error(w, "ERR Database error", http.StatusOK)
 		}
 		return
@@ -67,7 +67,7 @@ func decryptHandler(w http.ResponseWriter, r *http.Request) {
 
 	token, err := yotp.Parse(aesKey)
 	if err != nil {
-		log.Println("ERR Corrupt OTP:", otp)
+		glog.Info("ERR Corrupt OTP (Parse failed): ", otp)
 		http.Error(w, "ERR Corrupt OTP", http.StatusOK)
 		return
 	}
@@ -75,14 +75,14 @@ func decryptHandler(w http.ResponseWriter, r *http.Request) {
 	nameBytes, _ /* err */ := hex.DecodeString(name) // error ignored, we trust the database
 
 	if !bytes.Equal(nameBytes, token.Uid[:]) {
-		log.Println("ERR Corrupt OTP:", otp)
+		glog.Warning("ERR Corrupt OTP (UID mismatch): ", otp)
 		http.Error(w, "ERR Corrupt OTP", http.StatusOK)
 		return
 	}
 
 	response := fmt.Sprintf("OK counter=%04x low=%04x high=%02x use=%02x", token.Ctr, token.Tstpl, token.Tstph, token.Use)
 
-	log.Println(response)
+	glog.Info(response)
 	fmt.Fprintf(w, response)
 }
 
@@ -92,13 +92,13 @@ func main() {
 	flag.Parse()
 
 	if *dbfile == "" {
-		log.Fatalf("No database provided (-db)")
+		glog.Fatal("No database provided (-db)")
 	}
 
 	var err error
 	KeysDB, err = sql.Open("sqlite3", *dbfile)
 	if err != nil {
-		log.Fatalf("can't open sqlite3://%s: %s\n", *dbfile, err)
+		glog.Fatalf("can't open sqlite3://%s: %s\n", *dbfile, err)
 	}
 
 	http.HandleFunc("/wsapi/decrypt", decryptHandler)
@@ -107,6 +107,6 @@ func main() {
 	if p := os.Getenv("PORT"); p != "" {
 		port = ":" + p
 	}
-	log.Println("listening on port", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	glog.Info("listening on port ", port)
+	glog.Fatal(http.ListenAndServe(port, nil))
 }
